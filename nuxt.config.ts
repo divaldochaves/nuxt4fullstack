5d26894c59de947cb0f110e1887136a7588087fd
Divaldo Chaves <divaldo.chaves@gmail.com>
2026-05-07 19:54:29 -0300

    Capítulo 3 completado.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  css: ['~/assets/css/main.css']
 })
